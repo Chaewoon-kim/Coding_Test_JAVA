1e328af1ff77a5eaca108c7a2bb8128881efeb1f
Chaewoon-kim <149470542+Chaewoon-kim@users.noreply.github.com>
2024-04-30 14:39:24 +0900

    Update 대소문자 바꿔서 출력하기.java

--- Java/프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.java
+++ Java/프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.java
@@ -8,11 +8,13 @@ public class Solution {
         String answer = "";
         
         for(int i = 0; i < a.length() ; i++){
-            b = a.charAt(i);
-        
+            b = a.charAt(i); 
+            //charAt() = 문자열에서 문자 하나만 가져오는 함수
             if(Character.isUpperCase(b)){
+                // b가 대문자일때 , toLowerCase() 함수 사용해 소문자로 변환
                 answer+= Character.toLowerCase(b);
             }else{
+                // b가 소문자일때, toUpperCase() 함수 사용해 대문자로 변환 
                 answer+= Character.toUpperCase(b);
             }
         }
