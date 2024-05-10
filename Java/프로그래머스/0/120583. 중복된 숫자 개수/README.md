8451b758ec149c6c192e0b43959fdf1030dd5496
Chaewoon-kim <149470542+Chaewoon-kim@users.noreply.github.com>
2024-05-10 18:38:39 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.02 ms, Memory: 77.2 MB -BaekjoonHub

--- Java/프로그래머스/0/120583. 중복된 숫자 개수/README.md
+++ Java/프로그래머스/0/120583. 중복된 숫자 개수/README.md
@@ -1,10 +1,10 @@
 # [level 0] 중복된 숫자 개수 - 120583 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120583) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120583#) 
 
 ### 성능 요약
 
-메모리: 72.5 MB, 시간: 0.01 ms
+메모리: 77.2 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 10일 18:35:03
+2024년 05월 10일 18:38:37
 
 ### 문제 설명
 
--- Java/프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.java
+++ Java/프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.java
@@ -2,12 +2,12 @@ class Solution {
     public int solution(int[] array, int n) {
         int answer = 0;
         // 반복문으로 배열의 원소들을 출력해 n과 값 비교.
-        for(int i = 0 ; i < array.length; i++){
-            // 만약 배열의 원소가 정수 n과 값이 같다면.
-            if(array[i] == n){
+        for(int i : array){
+            if( i == n){
                 answer++;
             }
         }
         return answer;
     }
+        
 }
