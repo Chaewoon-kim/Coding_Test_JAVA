051d69b76dfc276834cd5fc9815232eea030c727
Chaewoon-kim <149470542+Chaewoon-kim@users.noreply.github.com>
2024-05-12 00:56:16 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.01 ms, Memory: 74.3 MB -BaekjoonHub

--- Java/프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
+++ Java/프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -3,7 +3,7 @@ class Solution {
         int max = 0;
         int left = 0;
         max = money / 5500 ; 
-        left = money - (max * 5500) ;
+        left = money % 5500 ;
         int [] answer = {max,left};
         return answer;
     }
