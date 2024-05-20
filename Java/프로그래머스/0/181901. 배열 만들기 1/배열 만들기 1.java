9b3174091d7f325f4db0bff4bbf173502e447eab
Chaewoon-kim <149470542+Chaewoon-kim@users.noreply.github.com>
2024-05-21 02:21:21 +0900

    [level 0] Title: 배열 만들기 1, Time: 2.07 ms, Memory: 70.1 MB -BaekjoonHub

--- Java/프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
+++ Java/프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -8,11 +8,12 @@ class Solution {
                 count++;
             }
         }
+        //2. 배열에 k의 배수 채우기 
         int [] answer = new int[count];
         int index =  0;
         for(int i = 1 ; i <= n ; i++){
             if(i % k == 0){
-                answer[index] = i;
+                answer[index] = i; //answer의 0번째 인덱스부터 오름차순으로 정렬
                 index++;
             }
         }
