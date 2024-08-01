85ddcb64f35539c3a280773d7c34529e0a8702d8
Chaewoon-kim <149470542+Chaewoon-kim@users.noreply.github.com>
2024-08-01 14:23:06 +0900

    [Bronze V] Title: A/B, Time: 208 ms, Memory: 17792 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        System.out.println((double)num1 / num2);
+    }
+}
