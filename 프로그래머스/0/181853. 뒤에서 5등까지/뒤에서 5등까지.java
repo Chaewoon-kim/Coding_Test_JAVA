b0f9a58f5fce9229a1c4c46973ee82785a57bf07
Chaewoon-kim <149470542+Chaewoon-kim@users.noreply.github.com>
2024-08-01 11:08:26 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 0.52 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -3,7 +3,9 @@ class Solution {
     public int[] solution(int[] num_list) {
         //배열 정렬
         Arrays.sort(num_list);
+        //5개의 수를 담을 새로운 배열 생성
         int [] answer = new int[5];
+        
         for(int i = 0; i < 5 ; i++){
             answer[i] = num_list[i];
         }
