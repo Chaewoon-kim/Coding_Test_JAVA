7955c984c2a3ae3ccbb2e3edc8b6e2aad25db917
Chaewoon-kim <149470542+Chaewoon-kim@users.noreply.github.com>
2024-08-05 10:34:03 +0900

    [Bronze III] Title: 최소, 최대, Time: 1588 ms, Memory: 318180 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 class Main{
     public static void main(String[]args){
-         Scanner sc = new Scanner(System.in);
+          Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(); //정수의 개수 받아와서
         int [] numbers = new int[n]; // 그 개수만큼의 배열 생성
         for(int i = 0 ; i < n ; i++){
@@ -14,8 +14,6 @@ class Main{
             if(numbers[i] > maxValue){
                 maxValue = numbers[i];
             }
-        }
-        for(int i = 1; i < n ; i++){
             if(numbers[i] < minValue){
                 minValue = numbers[i];
             }
