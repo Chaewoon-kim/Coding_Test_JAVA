8a10ccf207c139b69f2fc34b908aedf4c8910086
Chaewoon-kim <149470542+Chaewoon-kim@users.noreply.github.com>
2024-09-01 14:10:27 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 316 ms, Memory: 19148 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        for(int i = N ; i >= 1 ; i--){
+            for(int j = 1 ; j <= N - i ; j++){
+                System.out.print(" ");
+            }
+            for(int  k = 1; k <= 2 * i - 1; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
