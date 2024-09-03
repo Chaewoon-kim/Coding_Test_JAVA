0d2ee8c72403b9fb7b6883feba5224d077ee52a5
Chaewoon-kim <149470542+Chaewoon-kim@users.noreply.github.com>
2024-09-03 09:01:46 +0900

    [Bronze II] Title: 대표값2, Time: 164 ms, Memory: 17776 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Main{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        
+        int [] numArr = new int [5];
+        for(int i = 0 ; i < numArr.length; i++){
+            numArr[i] = sc.nextInt();
+        }
+        
+        int sum = 0;
+        for(int i = 0 ; i < numArr.length; i++){
+            sum += numArr[i];
+        }
+        //평균
+        int avg = sum / numArr.length;
+        System.out.println(avg);
+        
+        Arrays.sort(numArr);
+        //중앙값
+        System.out.println(numArr[2]);
+    }
+}
