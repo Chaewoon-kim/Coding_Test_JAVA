0ee3f856a5f32f6acbfeec819fc1ac087739473f
Chaewoon-kim <149470542+Chaewoon-kim@users.noreply.github.com>
2025-04-01 10:50:19 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 168 ms, Memory: 17572 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[]args){
+        
+        Scanner sc = new Scanner(System.in);
+        
+        String word = sc.next();
+        for(int i = 0 ; i < word.length() ; i++){
+            char ch = word.charAt(i);
+            if('A' <= ch && ch <= 'Z'){
+                System.out.print((char)('a' + ch - 'A'));
+            }else{
+                System.out.print((char)('A' + ch - 'a'));
+            }
+        }
+    }
+}
