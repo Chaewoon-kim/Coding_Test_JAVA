f1cbafe783677cf9f35ad076b07b5b0a514dab65
Chaewoon-kim <149470542+Chaewoon-kim@users.noreply.github.com>
2025-04-02 15:52:41 +0900

    [Silver V] Title: 문서 검색, Time: 192 ms, Memory: 17816 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.java
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -1,24 +1,16 @@
 import java.util.Scanner;
 
-class Main{
-    
-    public static void main(String [] args){
-        
+class Main {
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String doc = sc.nextLine();
         String word = sc.nextLine();
-        
-        int count = 0;
-        int startIndex = 0;
-        
-        while(true){
-            int findIndex = doc.indexOf(word , startIndex);
-            if(findIndex < 0){
-                break;
-            }
-            count++;
-            startIndex = findIndex + word.length();
-        }
+
+        String replaced = doc.replace(word, "");
+        int newLength = doc.length() - replaced.length();
+        int count = newLength / word.length();
         System.out.println(count);
     }
+
 }
