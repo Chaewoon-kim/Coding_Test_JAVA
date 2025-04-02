192eecdfc8b3051c76a047d197190bc6cc781fcd
Chaewoon-kim <149470542+Chaewoon-kim@users.noreply.github.com>
2025-04-02 12:12:34 +0900

    [Bronze II] Title: 애너그램 만들기, Time: 176 ms, Memory: 17576 KB -BaekjoonHub

---
+++ 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+
+class Main{
+    
+    public static int[] getAlphabetCount(String str){
+        int [] count = new int[26];
+        for(int i = 0; i < str.length(); i++){
+            count[str.charAt(i) - 'a']++;
+        }
+        return count;
+    }
+    
+    public static void main(String []args){
+        
+        Scanner sc = new Scanner(System.in);
+        
+        String a = sc.next();
+        String b = sc.next();
+        
+        int[] countA = getAlphabetCount(a);
+        int[] countB = getAlphabetCount(b);
+        
+    
+        int answer = 0;
+        
+        for(int i = 0 ; i < 26 ; i++){
+            answer += Math.abs(countA[i] - countB[i]);
+        }
+        
+        System.out.println(answer);
+    }
+}
