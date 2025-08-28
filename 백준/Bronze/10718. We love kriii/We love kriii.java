3446b71bdfa8c8a6ea079d9da990f774aa1e1ba1
Chaewoon-kim <149470542+Chaewoon-kim@users.noreply.github.com>
2025-08-28 09:27:28 +0900

    [Bronze V] Title: We love kriii, Time: 116 ms, Memory: 14648 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,12 @@
+class Main{
+    public static void main(String [] args){
+		
+		String cheerUp = "강한친구 대한육군";
+		String result = "";
+		for(int i = 0 ; i < 2; i++){
+			result+=cheerUp + "\n";
+		}
+		System.out.println(result);
+		
+	}
+}
