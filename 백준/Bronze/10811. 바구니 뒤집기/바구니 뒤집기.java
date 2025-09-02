302d88de316bf00b0c65ed7accd6ba6dc71d724b
Chaewoon-kim <149470542+Chaewoon-kim@users.noreply.github.com>
2025-09-02 13:44:48 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 120 ms, Memory: 15848 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -1,38 +1,43 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 
 class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-		int N = sc.nextInt();
-		int M = sc.nextInt();
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 
 		int temp = 0;
 
-		int [] buckets = new int [N];
-		//값 저장 
+		int [] bucket = new int[N];
+
 		for(int i = 0 ; i < N ; i++){
-			buckets[i] += i + 1;
+			bucket[i] = i + 1;
 		}
 
 		for(int i = 0 ; i < M ; i++){
-			int startNum = sc.nextInt() - 1;
-			int endNum = sc.nextInt() - 1;
+			st = new StringTokenizer(br.readLine(), " "); //1 2 
+			int startNum = Integer.parseInt(st.nextToken()) - 1; //StringTokinzer에서 토큰을 하나 꺼냄
+			int endNum = Integer.parseInt(st.nextToken()) - 1;
 
 			while(startNum < endNum){
-				temp = buckets[startNum];
-				buckets[startNum] = buckets[endNum];
-				buckets[endNum] = temp;
+				temp = bucket[startNum];
+				bucket[startNum] = bucket[endNum];
+				bucket[endNum] = temp;
 
-				startNum ++;
-				endNum --;
+				startNum++;
+				endNum--;
 			}
 		}
-
-		for(int i = 0 ; i < buckets.length ; i++){
-			System.out.print(buckets[i] + " ");
+		
+		for(int i = 0 ; i < bucket.length ; i++){
+			System.out.print(bucket[i] + " ");
 		}
 
 
